37d9403f0f56d119093b1f1171f3957e56fd6805
MrNobody0525 <930958138@qq.com>
2019-06-13 06:04:02 -0700

    create a knn.py file

---
+++ knn.py
@@ -0,0 +1,64 @@
+from numpy import *
+import operator 
+import sklearn.datasets as datasets
+from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
+#from sklearn.neighbors import KNeighborsClassifier
+
+import numpy as np
+import pandas as pd
+#from pandas import Series, DataFrame
+
+def createDataSet():
+    # 创建训练集
+    iris = datasets.load_iris()
+
+    group = iris['data']
+
+    labels = iris['target']
+
+    X_train,X_test,y_train,y_test = train_test_split(group,labels,test_size = 0.1)
+    
+    labels_names = iris['target_names']
+
+    return X_train, X_test,y_train,y_test,labels_names
+	
+def classify(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    result = []
+    # 根据欧式距离计算训练集中每个样本到测试点的距离
+    for i in inX:
+
+        diffMat = tile(i, (dataSetSize,1)) - dataSet
+        sqDiffMat = diffMat**2
+
+        sqDistances = sqDiffMat.sum(axis=1)
+        distances = sqDistances**0.5
+
+        # 计算完所有点的距离后，对数据按照从小到大的次序排序
+        sortedDistIndicies = distances.argsort()
+        # 确定前k个距离最小的元素所在的主要分类，最后返回发生频率最高的元素类别
+        classCount={}
+        for i in range(k):
+            voteIlabel = labels[sortedDistIndicies[i]]
+            classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+        sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+        result.append(int(sortedClassCount[0][0]))
+    return result
+
+def score(y_test,result):
+    i = 0
+    count = 0
+    while i < 15:
+        if y_test[i] == result[i]:
+            count +=1
+        i+=1
+    return float(count/i)
+        
+
+if __name__ == '__main__':
+    X_train,X_test,y_train,y_test,labels_names = createDataSet()
+    result = classify(X_test,X_train,y_train,10)
+    #计算正确率
+    Score = score(y_test,result)
+    print("正确率为：",Score)
